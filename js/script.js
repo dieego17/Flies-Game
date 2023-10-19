52ab546c93f2ee1672b9d90a5b226df47774ee5c
dieego17 <diegorubio1704.drs@gmail.com>
2023-10-19 17:03:12 +0200

    counter flies commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 const marcador = document.getElementById("marcador");
 const fondo_juego = document.getElementById("fondo_juego");
 
+let random = Math.floor(Math.random()*50);
 
 const inicioJuego = (event) => {
-    let random = Math.floor(Math.random()*50);
+    
     
     if(random >= 5){
         marcador.textContent = random;
@@ -13,12 +14,12 @@ const inicioJuego = (event) => {
             mosca.src = "./imagenes/mosca.png";
             let postLeft = Math.floor(Math.random() * (fondo_juego.clientWidth - mosca.width));
             let posTop = Math.floor(Math.random() * (fondo_juego.clientHeight - mosca.height));
-            mosca.setAttribute(
+           mosca.setAttribute(
                 "style",
                 "position:absolute; left:" + postLeft + "px; top:"+
                 posTop + "px"
-            ) 
-            
+            )
+            mosca.style.border = "1px solid red" 
             fondo_juego.appendChild(mosca)
 
         } 
@@ -32,7 +33,9 @@ const pulsarMosca = (event) => {
     let element = event.target;
 
     if(element.nodeName === "IMG"){
-        console.log(event)
+        element.remove()
+        marcador.textContent -= 1 
+        
     }
 
 }
