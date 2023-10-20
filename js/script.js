c152da261699ba579d9d8a72a2a07666627a86c0
dieego17 <diegorubio1704.drs@gmail.com>
2023-10-20 19:18:24 +0200

    the last commit

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ const marcador = document.getElementById("marcador");
 const fondo_juego = document.getElementById("fondo_juego");
 
 let random = Math.floor(Math.random()*50);
-
+let postLeft;
+let posTop;
 const inicioJuego = (event) => {
     
     
@@ -10,16 +11,19 @@ const inicioJuego = (event) => {
         marcador.textContent = random;
         for(let i = 0; i < random; i++){
         
+            do{
+                postLeft = Math.floor(Math.random() * (fondo_juego.clientWidth - 90));
+                posTop = Math.floor(Math.random() * (fondo_juego.clientHeight - 90));
+
+            }while(!validarPosiciones(postLeft, posTop));
+            
             let mosca = document.createElement("IMG");
             mosca.src = "./imagenes/mosca.png";
-            let postLeft = Math.floor(Math.random() * (fondo_juego.clientWidth - 90));
-            let posTop = Math.floor(Math.random() * (fondo_juego.clientHeight - 90));
-           mosca.setAttribute(
+            mosca.setAttribute(
                 "style",
                 "position:absolute; left:" + postLeft + "px; top:"+
                 posTop + "px"
-            )
-            mosca.style.border = "1px solid red" 
+            ) 
             fondo_juego.appendChild(mosca)
 
         } 
@@ -28,6 +32,33 @@ const inicioJuego = (event) => {
         window.location.reload();
         
     }
+}
+const validarPosiciones = (postLeft, posTop) => {
+
+    let posCorrecta = true;
+    let moscas = fondo_juego.querySelectorAll("IMG");
+    let i = 0;
+
+    while(i < moscas.length && posCorrecta){
+        //obtenemos la posicion de cada una de las moscas que tenemos
+        moscaLeft = parseInt(moscas[i].style.left);
+        moscaTop = parseInt(moscas[i].style.top);
+        if(
+            postLeft + 80 < moscaLeft ||
+            postLeft - 80 > moscaLeft ||
+            posTop + 80 < moscaTop ||
+            posTop - 80 > moscaTop
+        ){
+            posCorrecta = true;
+
+        }else{
+
+            posCorrecta = false;
+        }
+        i++;
+
+    }
+    return posCorrecta;
 }
 
 const pulsarMosca = (event) => {
